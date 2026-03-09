d68d151ab8258a17f6fa1deb2ac93d5df69b23f3
Aryan Shukla <196469984+aryan-astra@users.noreply.github.com>
2026-03-10 03:53:26 +0530

    UC6: Queue + Stack Based Palindrome Check

---
+++ src/UseCase6PalindromeCheckerApp.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class UseCase6PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "madam";
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        // Enqueue (FIFO) and push (LIFO) all characters
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        // Compare: dequeue gives original order, pop gives reversed order
+        boolean isPalindrome = true;
+        while (!queue.isEmpty()) {
+            if (!queue.poll().equals(stack.pop())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input          : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
