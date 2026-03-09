098673ebc6f4b31a3e4827505f77933dcc1659a7
Aryan Shukla <196469984+aryan-astra@users.noreply.github.com>
2026-03-10 03:52:02 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("===========================================");
+        System.out.println("   Welcome to Palindrome Checker App       ");
+        System.out.println("===========================================");
+        System.out.println("Application Name    : Palindrome Checker");
+        System.out.println("Application Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
